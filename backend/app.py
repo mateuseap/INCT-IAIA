7ff0cb5f4456c9af4797630b90a8224fbd03611e
Mateus Elias <meap@cin.ufpe.br>
2024-06-05 10:49:47 -0300

    feat(backend): create endpoint to retrieve a single researcher info

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
-from utils import get_all_xml_files, parse_research_xml
+from utils import (
+    get_all_xml_files,
+    get_one_xml_file,
+    parse_researcher_xml,
+    parse_researcher_name,
+)
 import json
 
 
@@ -15,22 +20,31 @@ def hello_world():
 
 @app.route("/get-researchers/<int:page>", methods=["GET"])
 def get_researchers(page):
-    xml_directory = "data/researchers"
-
-    all_xml_files = get_all_xml_files(xml_directory)
-    all_xml_files.sort()
-
+    all_xml_files = get_all_xml_files("data/researchers")
     selected_xml_files = all_xml_files[(page - 1) * 6 : page * 6]
 
     if not selected_xml_files:
         return json.dumps({"error": "No researchers found."}), 404
 
-    researchers = [parse_research_xml(file) for file in selected_xml_files]
-
+    researchers = [parse_researcher_xml(file) for file in selected_xml_files]
     researchers_dict = [researcher.to_dict() for researcher in researchers]
     response_body = json.dumps(researchers_dict, ensure_ascii=False).encode(
         "ISO-8859-1"
     )
+
+    return response_body, 200, {"Content-Type": "application/json; charset=ISO-8859-1"}
+
+
+@app.route("/get-researcher/<string:name>", methods=["GET"])
+def get_researcher(name):
+    xml_file = get_one_xml_file("data/researchers", parse_researcher_name(name))
+
+    if not xml_file:
+        return json.dumps({"error": "Researcher not found."}), 404
+
+    researcher = parse_researcher_xml(xml_file, all_info=True)
+    researcher_dict = researcher.to_dict()
+    response_body = json.dumps(researcher_dict, ensure_ascii=False).encode("ISO-8859-1")
 
     return response_body, 200, {"Content-Type": "application/json; charset=ISO-8859-1"}
 
--- backend/utils.py
+++ backend/utils.py
@@ -1,6 +1,7 @@
 import os
 import base64
 import xml.etree.ElementTree as ET
+from classes.journal_article import JournalArticle
 from classes.researcher import Researcher
 
 
@@ -8,7 +9,14 @@ def convert_image_to_base64(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
 
-def parse_research_xml(file_path):
+
+def parse_researcher_journals_articles(researcher, article_elements):
+    for article_element in article_elements:
+        article = JournalArticle.from_xml(article_element)
+        researcher.add_journal_article(article)
+
+
+def parse_researcher_xml(file_path, all_info=False):
     parser = ET.XMLParser(encoding="ISO-8859-1")
     tree = ET.parse(file_path, parser=parser)
     root = tree.getroot()
@@ -34,20 +42,50 @@ def parse_research_xml(file_path):
     ]
     profile_pic = None
     for extension in profile_pic_extensions:
-        profile_pic_path = os.path.join(os.path.dirname(file_path), "profilepic" + extension)
+        profile_pic_path = os.path.join(
+            os.path.dirname(file_path), "profilepic" + extension
+        )
         if os.path.isfile(profile_pic_path):
             profile_pic = convert_image_to_base64(profile_pic_path)
             break
 
-    return Researcher(id, name, resume_text, profile_pic)
+    researcher = Researcher(id, name, resume_text, profile_pic)
+
+    if all_info:
+        article_elements = root.findall(".//ARTIGO-PUBLICADO")
+        parse_researcher_journals_articles(researcher, article_elements)
+
+    return researcher
 
 
-def get_all_xml_files(directory):
-    import os
+def parse_researcher_name(name):
+    return " ".join(
+        [
+            word.capitalize() if word not in ["de", "da", "dos"] else word
+            for word in name.split("-")
+        ]
+    )
 
+
+def get_all_xml_files(base_path):
     xml_files = []
-    for root, dirs, files in os.walk(directory):
+
+    for root, dirs, files in os.walk(base_path):
         for file in files:
             if file.endswith(".xml"):
                 xml_files.append(os.path.join(root, file))
+
+    xml_files.sort()
+
     return xml_files
+
+
+def get_one_xml_file(base_path, name):
+    researcher_path = os.path.join(base_path, name)
+
+    if not os.path.isdir(researcher_path):
+        return None
+
+    xml_file = os.path.join(researcher_path, "curriculo.xml")
+
+    return xml_file
