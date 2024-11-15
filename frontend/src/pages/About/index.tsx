13035189acf68ab59508176da3a8282f32df0365
Mateus Elias <meap@cin.ufpe.br>
2024-11-15 11:29:17 -0300

    feat(frontend/src/pages/About): add new content and change some styles

--- frontend/src/pages/About/index.tsx
+++ frontend/src/pages/About/index.tsx
@@ -5,112 +5,177 @@ export default function About() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <Navbar />
-      <main className="isolate sm:mb-8 mb-14 flex-grow">
+      <main className="mt-12 isolate sm:mb-8 mb-14 flex-grow">
         <div className="relative pt-14">
-          <div
-            className="absolute inset-x-0 -top-90 -z-10 transform-gpu overflow-hidden blur-xl sm:-top-80 opacity-40"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-            />
-          </div>
           <div className="py-4 sm:py-8 text-center">
             <h1 className="lg:text-4xl text-2xl font-bold tracking-tight text-gray-900">
-              About
+              Brazilian National Institute
+              <br />
+              of Artificial Intelligence
             </h1>
           </div>
-          <div className="flex flex-col justify-center mx-auto items-center max-w-4xl gap-y-8 px-4">
-            <div className="flex flex-col gap-y-4 text-left">
-              <h1 className="lg:text-2xl text-xl font-bold tracking-tight text-gray-900">
+          <div className="flex flex-col justify-center mx-auto max-w-4xl gap-y-6 px-4">
+            <div className="flex flex-col gap-y-2 text-left">
+              <h1 className="lg:text-xl text-lg font-bold tracking-tight">
                 Our Vision
               </h1>
-              To be at the forefront of research and innovation in Artificial
-              Intelligence (AI), ensuring its responsible deployment that aligns
-              harmoniously with human values and societal needs.
+              <span className="text-justify">
+                To lead in research and innovation in Artificial Intelligence
+                (AI), promoting responsible deployment that aligns seamlessly
+                with human values and societal needs.
+              </span>
             </div>
-            <div className="flex flex-col gap-y-4 text-left">
-              <h1 className="lg:text-2xl text-xl font-bold tracking-tight text-gray-900">
+            <div className="flex flex-col gap-y-2 text-left">
+              <h1 className="lg:text-xl text-lg font-bold tracking-tight">
                 Our Mission
               </h1>
-              <div className="flex flex-col gap-y-2">
-                <span>
-                  • To address the paramount challenges of integrating AI into
-                  various facets of human life, ensuring its transparency,
-                  privacy, security, responsibility, reliability, fairness, and
-                  explainability.
+              <div className="flex flex-col gap-y-2 ml-4 text-justify">
+                <span className="flex gap-x-1">
+                  <span className="text-xl font-bold">•</span>Address the
+                  crucial challenges of integrating AI into various aspects of
+                  life, ensuring transparency, privacy, security,
+                  responsibility, reliability, fairness, and explainability.
                 </span>
-                <span>
-                  • To champion responsible AI by rigorously addressing
-                  technical challenges, such as managing intricate data sets,
-                  ensuring robustness against noise and biases, and advancing
-                  innovative model structures.
+                <span className="flex gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  Advocate for responsible AI by tackling technical challenges
+                  such as complex data management, resilience against noise and
+                  biases, and advancing innovative model architectures.
                 </span>
-                <span>
-                  • To underscore the significance of continuous data
-                  collection, integration, and the training of professionals,
-                  ensuring they're equipped to navigate the constantly evolving
-                  realm of AI.
+                <span className="flex gap-x-1">
+                  <span className="text-xl font-bold">•</span>Highlight the
+                  importance of continuous data collection, integration, and
+                  professional training, ensuring readiness to navigate AI's
+                  rapidly evolving landscape.
                 </span>
-                <span>
-                  • To bridge the gap between theoretical AI models and their
-                  practical application, ensuring maximum societal benefit and
-                  minimal risks.
+                <span className="flex gap-x-1">
+                  <span className="text-xl font-bold">•</span>Bridge the gap
+                  between theoretical AI models and practical applications,
+                  maximizing societal benefits and minimizing risks.
                 </span>
               </div>
             </div>
-            <div className="flex flex-col gap-y-4 text-left">
-              <h1 className="lg:text-2xl text-xl font-bold tracking-tight text-gray-900">
+            <div className="flex flex-col gap-y-2 text-left">
+              <h1 className="lg:text-xl text-lg font-bold tracking-tight">
                 Our Values
               </h1>
-              <div className="flex flex-col gap-y-2">
-                <span>
-                  • Innovation: Constantly pushing the boundaries of what's
-                  possible in AI research.
+              <div className="flex flex-col gap-y-2 ml-4">
+                <span className="flex items-center gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  <span className="font-bold">Innovation:</span>
+                  Continuously pushing the boundaries of AI research.
                 </span>
-                <span>
-                  • Integrity: Maintaining transparency and honesty in all our
-                  endeavors.
+                <span className="flex items-center gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  <span className="font-bold">Integrity:</span> Upholding
+                  transparency and honesty in every endeavor.
                 </span>
-                <span>
-                  • Collaboration: Engaging with diverse stakeholders, from
-                  government bodies to the general public, to ensure AI serves
-                  everyone.
+                <span className="flex items-center gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  <span className="font-bold">Collaboration:</span> Engaging
+                  diverse stakeholders, from government to the public, to ensure
+                  AI benefits everyone.
                 </span>
-                <span>
-                  • Excellence: Striving for the highest standards in research,
-                  training, and application.
+                <span className="flex items-center gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  <span className="font-bold">Excellence:</span>
+                  Striving for the highest standards in research, training, and
+                  application.
                 </span>
-                <span>
-                  • Responsibility: Prioritizing the ethical considerations and
-                  societal impacts of AI.
+                <span className="flex items-center gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  <span className="font-bold">Responsibility:</span>
+                  Prioritizing the ethical considerations and societal impacts
+                  of AI.
                 </span>
               </div>
             </div>
-            <div className="flex flex-col gap-y-4 text-left">
-              <h1 className="lg:text-2xl text-xl font-bold tracking-tight text-gray-900">
+            <div className="flex flex-col gap-y-2 text-left">
+              <h1 className="lg:text-xl text-lg font-bold tracking-tight">
                 Our History
               </h1>
-              Founded in 2023, the National Institute of Artificial Intelligence
-              (IAIA) is quickly becoming a beacon of excellence in the world of
-              AI. Rooted in Brazil, we've collaborated with international
-              bodies, governments, and industries to advance the safe and
-              beneficial use of AI. Our legacy is built on pioneering research,
-              groundbreaking innovations, and a commitment to shaping the future
-              of AI in a way that benefits humanity as a whole.
+              <span className="text-justify">
+                Founded in 2023, the National Institute of Artificial
+                Intelligence (IAIA) has rapidly become a leading force in AI.
+                Headquartered in Brazil, IAIA collaborates globally with
+                governments, industries, and institutions to foster the safe and
+                beneficial use of AI. Built on pioneering research and
+                innovation, our legacy is committed to shaping an AI-driven
+                future that serves humanity.
+              </span>
             </div>
-            <div className="flex flex-col gap-y-4 text-left">
-              <h1 className="lg:text-2xl text-xl font-bold tracking-tight text-gray-900">
+            <div className="flex flex-col gap-y-2 text-left">
+              <h1 className="lg:text-xl text-lg font-bold tracking-tight">
                 Join Us
               </h1>
-              Whether you're a student looking to delve into the fascinating
-              world of AI, a professional seeking collaboration, or a curious
-              individual wanting to understand more about the technology that's
-              reshaping our world - the IAIA welcomes you.
+              <span className="text-justify">
+                Whether you're a student eager to explore AI, a professional
+                seeking collaboration, or a curious individual wanting to learn
+                about the technology reshaping our world, IAIA welcomes you.
+              </span>
+            </div>
+            <div className="flex flex-col gap-y-2 text-left">
+              <h1 className="lg:text-xl text-lg font-bold tracking-tight">
+                Governing Committee
+              </h1>
+              <div className="flex flex-col gap-y-2 ml-4">
+                <span className="flex gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  <span className="flex flex-col">
+                    Teresa Ludermir
+                    <i>(UFPE, Coordinator)</i>
+                  </span>
+                </span>
+                <span className="flex gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  <span className="flex flex-col">
+                    Cleber Zanchettin
+                    <i>(UFPE, Executive Coordinator)</i>
+                  </span>
+                </span>
+                <span className="flex gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  <span className="flex flex-col">
+                    Wagner Meira
+                    <i>(UFMG, Vice-Coordinator)</i>
+                  </span>
+                </span>
+                <span className="flex gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  <span className="flex flex-col">
+                    André Carvalho
+                    <i>(USP, Vice-Coordinator)</i>
+                  </span>
+                </span>
+                <span className="flex gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  <span className="flex flex-col">
+                    Altigran Silva
+                    <i>(UFAM)</i>
+                  </span>
+                </span>
+                <span className="flex gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  <span className="flex flex-col">
+                    Carmelo Bastos Filho
+                    <i>(UPE)</i>
+                  </span>
+                </span>
+                <span className="flex gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  <span className="flex flex-col">
+                    Francisco Carvalho
+                    <i>(UFPE)</i>
+                  </span>
+                </span>
+                <span className="flex gap-x-1">
+                  <span className="text-xl font-bold">•</span>
+                  <span className="flex flex-col">
+                    Marley Vellasco
+                    <i>(PUC-RJ)</i>
+                  </span>
+                </span>
+              </div>
             </div>
           </div>
         </div>
